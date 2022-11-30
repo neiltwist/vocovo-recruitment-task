08d46e7a6f0039dc2598cde4ffcc37dc0591ad41
Neil Twist <1476891+neiltwist@users.noreply.github.com>
2022-11-30 15:16:56 +0000

    REST interfaces

--- api/appliances/src/db.ts
+++ api/appliances/src/db.ts
@@ -1,4 +1,5 @@
 import crypto from 'crypto'
+import { isNullOrUndefined } from 'util'
 
 function randomDate(start: Date, end: Date) {
   return new Date(
@@ -6,9 +7,21 @@ function randomDate(start: Date, end: Date) {
   )
 }
 
+type Appliance = {
+  id: number
+  name: string
+  type: string
+  createdAt: Date
+}
+
+type ApplianceUpdate = {
+  name?: string
+  type?: string
+}
+
 const types = ['Set top box', 'Lightbulb', 'Smoke detector']
 
-const appliances = []
+const appliances: Array<Appliance> = []
 
 for (let i = 0; i < 100; i++) {
   appliances.push({
@@ -17,6 +30,45 @@ for (let i = 0; i < 100; i++) {
     type: types[Math.floor(Math.random() * types.length)],
     createdAt: randomDate(new Date(2022, 0, 1), new Date(2022, 0, 30)),
   })
+}
+
+export function findById(id: number) {
+  return appliances.find((thisAppliance) => thisAppliance.id === id)
+}
+
+export function createNew(): Appliance {
+  const newId = appliances.length
+  const newAppliance = {
+    id: newId,
+    name: `Appliance ${crypto.randomBytes(2).toString('hex')}`,
+    type: types[Math.floor(Math.random() * types.length)],
+    createdAt: new Date(),
+  }
+  appliances.push(newAppliance)
+  return newAppliance
+}
+
+export function update(
+  appliance: Appliance,
+  fieldsToUpdate: ApplianceUpdate
+): Appliance {
+  const index = appliances.findIndex(
+    (thisAppliance) => thisAppliance.id === appliance.id
+  )
+
+  Object.keys(fieldsToUpdate)
+    .filter(
+      (key: string) =>
+        appliance.hasOwnProperty(key) &&
+        fieldsToUpdate[key] !== null &&
+        fieldsToUpdate[key] !== undefined
+    )
+    .forEach((key: string) => {
+      appliance[key] = fieldsToUpdate[key as keyof typeof fieldsToUpdate]
+    })
+
+  appliances[index] = appliance
+  return appliance
 }
 
 export default appliances
--- api/appliances/src/routes.ts
+++ api/appliances/src/routes.ts
@@ -1,6 +1,10 @@
 import express, { Request, Response } from 'express'
 
-import applianceDB from './db'
+import applianceDB, {
+  createNew as createNewAppliance,
+  findById as findApplianceById,
+  update as updateAppliance,
+} from './db'
 
 const router = express.Router()
 
@@ -8,4 +12,65 @@ router.get('/list', (req: Request, res: Response) => {
   res.json(applianceDB)
 })
 
+// read
+router.get('/:applianceId(\\d+)', (req: Request, res: Response) => {
+  const applianceId = parseInt(req.params.applianceId)
+  const appliance = findApplianceById(applianceId)
+  if (!appliance) {
+    res.status(404).json({ message: `appliance not found` })
+    return
+  }
+  res.json(appliance)
+})
+
+//create
+router.post('/', (req: Request, res: Response) => {
+  // name, type
+  const {
+    applianceName,
+    applianceType,
+  }: { applianceName?: string; applianceType?: string } = req.body
+  const appliance = createNewAppliance()
+  const appliancePath = `${req.path}${appliance.id}`
+  res.status(201).location(appliancePath).json(appliance)
+})
+
+//update
+router.put('/:applianceId(\\d+)', (req: Request, res: Response) => {
+  // id, name, type
+  const applianceId = parseInt(req.params.applianceId)
+  const { name, type }: { name?: string; type?: string } = req.body
+  const appliance = findApplianceById(applianceId)
+  if (!appliance) {
+    res.status(404).json({ message: `appliance not found` })
+    return
+  }
+  const updatedAppliance = updateAppliance(appliance, { name, type })
+  const appliancePath = `${req.path}${updatedAppliance.id}`
+  res.status(201).location(appliancePath).json(updatedAppliance)
+})
+
 export default router
+
+/*
+fetch('https://vocovorecruitmenttask-cnlx--3000.local-credentialless.webcontainer.io/appliances/', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+}).then((response) => {console.log(response.status, response.headers.get('Location'));
+  return response.json();
+}).then(console.log);
+*/
+
+/*
+fetch('https://vocovorecruitmenttask-cnlx--3000.local-credentialless.webcontainer.io/appliances/10', {
+  method: 'PUT',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({name: "new name", type: "new type"}),
+}).then((response) => {console.log(response.status, response.headers.get('Location'));
+  return response.json();
+}).then(console.log);
+*/
