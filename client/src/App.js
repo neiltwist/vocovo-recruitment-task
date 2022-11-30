213980f6d023a922cf21142e8cef51a11ce1942c
Neil Twist <1476891+neiltwist@users.noreply.github.com>
2022-11-30 22:19:53 +0000

    Styling and Sorting

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,69 @@
 import { useEffect, useState } from 'react'
 import moment from 'moment'
 
+import './App.css'
+
 const APPLIANCES_LIST_URL = 'http://localhost:8080/appliances/list'
 const DATE_FORMAT = 'YYYY-MM-DD HH:mm:ss'
 
 function Appliance({ id, name, type, createdAt }) {
   return (
-    <div key={id}>
-      name: {name}, type: {type}, created:{' '}
-      {moment(createdAt).format(DATE_FORMAT)}
-    </div>
+    <>
+      <td>{id}</td>
+      <td>{name}</td>
+      <td>{type}</td>
+      <td>{moment(createdAt).format(DATE_FORMAT)}</td>
+    </>
   )
 }
 
 function ApplianceList() {
   const [appliances, setAppliances] = useState([])
+  const [sortBy, setSortBy] = useState(null)
   useEffect(() => {
     fetch(APPLIANCES_LIST_URL)
       .then((response) => {
-        console.log(response.status, response.headers.get('Location'))
         return response.json()
       })
       .then(setAppliances)
   }, [])
+
+  const compareNumbers = (a, b) => a[sortBy] - b[sortBy]
+  const compareStrings = (a, b) =>
+    a[sortBy] < b[sortBy] ? -1 : a[sortBy] > b[sortBy] ? 1 : 0
+  const compareDates = (a, b) =>
+    moment(a[sortBy]).isBefore(b[sortBy])
+      ? -1
+      : moment(a[sortBy]).isBefore(b[sortBy])
+      ? 1
+      : 0
+
+  const sortedAppliances =
+    sortBy !== null
+      ? sortBy === 'id'
+        ? appliances.slice().sort(compareNumbers)
+        : sortBy === 'createdAt'
+        ? appliances.slice().sort(compareDates)
+        : appliances.slice().sort(compareStrings)
+      : appliances
   return (
-    <ul>
-      {appliances.map((appliance) => (
-        <li>
-          <Appliance {...appliance} />
-        </li>
-      ))}
-    </ul>
+    <table>
+      <thead>
+        <tr>
+          <th onClick={() => setSortBy('id')}>ID</th>
+          <th onClick={() => setSortBy('name')}>Name</th>
+          <th onClick={() => setSortBy('type')}>Type</th>
+          <th onClick={() => setSortBy('createdAt')}>Created At</th>
+        </tr>
+      </thead>
+      <tbody>
+        {sortedAppliances.map((appliance, index) => (
+          <tr key={index}>
+            <Appliance {...appliance} />
+          </tr>
+        ))}
+      </tbody>
+    </table>
   )
 }
 
